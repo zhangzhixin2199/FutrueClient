cb77b74a2a68a9e06801746c6bc32adb654faf27
jasonzhang <zhangzhixin@58.com>
2018-03-26 15:00:17 +0800

    fix package name

---
+++ app/src/main/java/com/zzx/futrue/FutrueApplication.java
@@ -0,0 +1,10 @@
+package com.zzx.futrue;
+
+import android.app.Application;
+
+/**
+ * Created by jasonzhang on 18/3/26.
+ */
+
+public class FutrueApplication extends Application {
+}
--- app/src/main/java/com/zzx/futrue/MainActivity.java
+++ app/src/main/java/com/zzx/futrue/MainActivity.java
@@ -1,4 +1,4 @@
-package com.zzx.futrue.futrueclient;
+package com.zzx.futrue;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.zzx.futrue.futrueclient.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
