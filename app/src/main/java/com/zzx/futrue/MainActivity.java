a633a2f9f37f6aab4950b88ad22c84401a91380a
jasonzhang <zhangzhixin@58.com>
2018-03-26 15:02:28 +0800

    fix package name

--- app/src/main/java/com/zzx/futrue/MainActivity.java
+++ app/src/main/java/com/zzx/futrue/MainActivity.java
@@ -13,7 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.zzx.futrue.futrueclient.R;
+import com.zzx.futrue.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
